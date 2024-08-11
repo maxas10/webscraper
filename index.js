1bdf7163ba1a3f0813131668124603bd624dbb29
maxas10 <maxaseung@gmail.com>
2024-08-11 03:02:18 -0700

    faster runtime

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const app = express()
 const port = 3000
 const puppeteer = require("puppeteer");
 
-app.get('/', async (req, res) => {
+async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const newImgLinks = [];
-
-    // Set screen size.
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Navigate the page to a URL.
-    await page.goto(`https://www.pinterest.com/search/pins/?q=${req.query.query}`);
+    app.get('/', async (req, res) => {
+        const newImgLinks = [];
+        await page.goto(`https://www.pinterest.com/search/pins/?q=${req.query.query}`);
 
-    await page.waitForNetworkIdle(2000)
+        await page.waitForFunction(() => {
+            return document.querySelectorAll('img').length >= 15;
+        });
 
+        // Type into search box.
+        const imgSelector = await page.evaluate(() => Array.from(document.querySelectorAll("img"), e => e.src))
 
-    // Type into search box.
-    const imgSelector = await page.evaluate(() => Array.from(document.querySelectorAll("img"), e => e.src))
+        for (let link of imgSelector) {
+            var linkSplit = link.split('/')
+            linkSplit[3] = "originals"
+            newImgLinks.push(linkSplit.join('/'));
+        }
 
-    for (let link of imgSelector) {
-        var linkSplit = link.split('/')
-        linkSplit[3] = "originals"
-        newImgLinks.push(linkSplit.join('/'));
-    }
+        res.send(newImgLinks);
+    })
+}
 
-    await browser.close();
-
-    res.send(newImgLinks);
-})
+run();
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
