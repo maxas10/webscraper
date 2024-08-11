9c5f9fa31fc973c6eb536edef2d6f25e4e951009
maxas10 <maxaseung@gmail.com>
2024-08-11 03:37:36 -0700

    better image searching

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express()
 const port = 3000
 const puppeteer = require("puppeteer");
 
+require("dotenv").config()
+
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1024 });
+    await page.setCookie({
+        name: "_pinterest_sess",
+        url: "https://www.pinterest.com/",
+        value: process.env["_pinterest_sess"]
+    })
 
     app.get('/', async (req, res) => {
         const newImgLinks = [];
